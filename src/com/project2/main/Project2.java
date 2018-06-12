ef31122d116102d60260b069eb466c36bbc9a2b9
Royervivaelrock <rogelioflor3s@gmail.com>
2018-06-12 15:23:53 -0500

    super proyecto

---
+++ src/com/project2/main/Project2.java
@@ -0,0 +1,10 @@
+package com.project2.main;
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("new project");
+		System.out.println("new project 333");
+	}
+
+}
